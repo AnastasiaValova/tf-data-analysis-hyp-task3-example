921c02067a7fa1e15344abff0a034165621bbe49
Anastasia <33247868+AnastasiaValova@users.noreply.github.com>
2023-04-12 20:57:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -6,6 +6,6 @@ from scipy import stats
 chat_id = 225497605 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    alpha = 0.07
+    alpha = 0.09
     stat, p_value = stats.ttest_ind(x, y)
     return ( p_value < alpha)
